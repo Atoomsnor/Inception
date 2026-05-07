62b3ab4c9822f81e95c228c78d5e5a6ab7b538e6
Atoomsnor <verschoor.rowan@gmail.com>
2026-05-07 12:10:03 +0200

    start/end of port swapping, added mariadb healthcheck

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Waiting for mariadb..."
-sleep 3
-
 mkdir -p /var/www/html
 cd /var/www/html
 
@@ -20,19 +17,20 @@ wp config create \
 --allow-root \
 --skip-check
 
-wp core install \
---url=${DOMAIN_NAME} \
---title=${WP_TITLE} \
---admin_user=${WP_ADMIN_USER} \
---admin_password=${WP_ADMIN_PASSWORD} \
---admin_email=${WP_ADMIN_EMAIL} \
---allow-root
+if ! wp core is-installed --allow-root 2>/dev/null; then
+	wp core install \
+	--url=${DOMAIN_NAME} \
+	--title=${WP_TITLE} \
+	--admin_user=${WP_ADMIN_USER} \
+	--admin_password=${WP_ADMIN_PASSWORD} \
+	--admin_email=${WP_ADMIN_EMAIL} \
+	--allow-root
 
-wp user create ${WP_USER} ${WP_USER_EMAIL} \
---role=subscriber \
---user_pass=${WP_USER_PASSWORD} \
---allow-root
-
+	wp user create ${WP_USER} ${WP_USER_EMAIL} \
+	--role=subscriber \
+	--user_pass=${WP_USER_PASSWORD} \
+	--allow-root
+fi
 
 sed -i "s#listen = /run/php/php8.2-fpm.sock#listen = 0.0.0.0:9000#" /etc/php/8.2/fpm/pool.d/www.conf
 
